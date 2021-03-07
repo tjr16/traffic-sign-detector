1b51dd5274abad50ddda1fb2c119aa9274e8648c
jiaruit <jiaruit@kth.se>
2021-03-07 15:46:57 +0100

    Fix config dictionary

--- config.py
+++ config.py
@@ -13,13 +13,12 @@ CATEGORY_DICT = {
     6: {"name": "__CLASS 6__"},
     7: {"name": "__CLASS 7__"},
     8: {"name": "__CLASS 8__"},
-    9: {"name": "__CLASS 8__"},
+    9: {"name": "__CLASS 9__"},
     10: {"name": "residential"},
     11: {"name": "narrows_from_left"},
     12: {"name": "narrows_from_right"},
     13: {"name": "roundabout"},
-    14: {"name": "__CLASS 14__"},
-    15: {"name": "__CLASS 15__"},
+    14: {"name": "__CLASS 14__"}
 }
 
 BATCH_SIZE = 8
